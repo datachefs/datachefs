c7242de4d48faaa03799b0b0b799f49474b8a6eb
aschneiderman <aschneiderman@gmail.com>
2019-07-17 17:12:48 -0400

    Work on Moore's law animation

--- pages/babylonjs/dataviz/moores-law/myScript.js
+++ pages/babylonjs/dataviz/moores-law/myScript.js
@@ -36,9 +36,9 @@ var createScene = function () {
         BABYLON.SceneLoader.ImportMesh("", "", "robot-head.glb", scene, function (meshes) {          
 
         var robot =  scene.getMeshByID("node_id5");
-        // var  scale = (computing[0].transistors /  computing[lastYear].transistors) / 2;
-        robot.scaling =   new BABYLON.Vector3(0.1, 0.1, 0.1);
-        // scene.getMeshByID("node_id5").scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+        robot.position.y = -7;
+        var scale = 0.1;
+        scene.getMeshByID("node_id5").scaling = new BABYLON.Vector3(scale, scale, scale);
         var titleLabel = simpleTextBlock("Why Moore's Law Matters", {x: 0, y: 7, z:0,
             fontSize: "12px", width: "150px", height:  "150px"  }, scene);   
         var yearLabel = simpleTextBlock('Test', {x: 0, y: 6, z:0,
@@ -47,16 +47,71 @@ var createScene = function () {
             fontSize: "24px", width: "150px", height:  "150px"  }, scene);   
 
 
-        for (let j = 0; j < computing.length; j++) {
-            console.log(j, ': Scale is ', calculateScale(j));
+        // for (let j = 0; j < computing.length; j++) {
+        //     console.log(j, ': Scale is ', calculateScale(j));
             
-        }
-        var scale = 0.1;
+        // }
+
+        var maxScale = 1.25;
         var i = 0;
 
         yearLabel.text = 'Year:' +  computing[i].year;        
         frequencyLabel.text = computing[i].transistors.toLocaleString();
 
+        var yearScale = (computing[i].transistors / computing[lastYear].transistors) * maxScale;
+        console.log(i, 'scale:', yearScale)
+        
+    //     var robotAnimation = new BABYLON.Animation("robot", "robot.scale.y", 30, 
+    //     BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+    //     var keys = [];
+    //     keys.push({frame: 0, value: 0.1});
+    //     keys.push({frame: 60, value: 0.6});
+    //    // keys.push({frame: 0, value: robot.scale});
+    //     // keys.push({frame: 60, value: yearScale});
+    //     // keys.push({frame: 0, value: new BABYLON.Vector3(0.1, 0.1, 0.1)});
+    //     // keys.push({frame: 60, value: new BABYLON.Vector3(0.7, 0.7, 0.7)});
+    //     robotAnimation.setKeys(keys);
+    //     scene.beginAnimation(robot, 0, 100, true);
+
+    var plane = BABYLON.Mesh.CreatePlane("ground1", 2 , scene);
+
+    var animation = new BABYLON.Animation('animation', 'scaling', 60, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    // An array with all animation keys
+    var keyframes = [
+        {
+            frame: 0,
+            value: new BABYLON.Vector3(1, 1, 1)
+        },
+        {
+            frame: 120,
+            value: new BABYLON.Vector3(2, 2, 2)
+        }
+    ];
+
+    animation.setKeys(keyframes);
+
+    plane.animations = [animation];
+
+   scene.beginAnimation(plane, 0, 120, true);
+
+   var box = BABYLON.Mesh.CreateBox("myBox", 2, scene);
+   box.position = new BABYLON.Vector3(0, -10, 0);
+   var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+   myMaterial.diffuseColor =  new BABYLON.Color3.Teal();
+   box.material = myMaterial;
+
+   var animationBox = new BABYLON.Animation("movingBox", "position.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                                                                   BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+   // Animation keys
+   var keys = [];
+   keys.push({ frame: 0, value: -10 });
+   keys.push({ frame: 20, value: 20 });
+
+   animationBox.setKeys(keys);
+   box.animations.push(animationBox);
+
+   scene.beginAnimation(box, 0, 100, true);
 
         // scale = 0.1;
         // // scale = (computing[i].transistors /  computing[lastYear].transistors) / 2;
@@ -75,7 +130,6 @@ var createScene = function () {
 
 // BABYLON.Animation.ANIMATIONTYPE_VECTOR3
 
-// var xSlide = new BABYLON.Animation("xSlide", "position.x", frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
 
 // var yRot = new BABYLON.Animation("yRot", "rotation.y", frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
 
